9a6d0e0d5106c5deab31e508c9334a7688708231
Nemi Banigo <nemi@Nemis-MacBook-Air.local>
2017-08-02 18:18:35 +0100

    created functions for timer and start button

---
+++ stopwatch.js
@@ -0,0 +1,45 @@
+const startButton = document.querySelector('[data-action=start]');
+const stopButton= document.querySelector('[data-action=stop]');
+const resetButton = document.querySelector('[data-action=reset]');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+let timerTime = 00;
+let interval;
+
+
+
+
+//functions
+function keepzero(num){
+   return(num < 10) ? '0' + num  : num;
+}
+
+function startTimer(){
+    interval = setInterval(incrementTimer,1000);
+    }
+
+
+function stopTimer (){
+
+}
+function resetTimer(){
+
+}
+function incrementTimer(){
+    timerTime++;
+    const numOfminutes = Math.floor(timerTime/60);
+    const numOfseconds = timerTime % 60;
+    // console.log({numOfminutes,numOfseconds});
+    seconds.innerText =keepzero(numOfseconds);
+    minutes.innerText = keepzero(numOfminutes);
+}
+
+
+
+
+
+// add eventlisteners //
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
